6241acdd0aae2d67d285628bec527a8abdbcb714
Dorin <dorin.zahariazd@gmail.com>
2023-11-14 14:51:08 +0200

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(3000);
+mongoose
+  .connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.8zuvkr1.mongodb.net/?retryWrites=true&w=majority`)
+  .then((result) => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
