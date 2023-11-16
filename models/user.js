af917537af47ca92686db47d8da3d3cde74ea2be
Dorin <dorin.zahariazd@gmail.com>
2023-11-16 19:46:45 +0200

    Add User schema

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
