2aae480ed78a502d49af196fb7eee063257ca728
Dorin <dorin.zahariazd@gmail.com>
2023-11-17 17:09:24 +0200

    Add dummy create user route and validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,26 @@
-exports.getUsers = (req, res, next) => {
+const { validationResult } = require('express-validator');
+
+exports.getUser = (req, res, next) => {
   res.status(200).json({
     firstName: 'firstName',
     lastName: 'lastName',
+  });
+};
+
+exports.createUser = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      message: 'Validation failed',
+      errors: errors.array(),
+    });
+  }
+  console.log(errors);
+  const firstName = req.body.firstName;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  res.status(201).json({
+    message: 'User created successfully',
   });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const userController = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/:id', userController.getUsers);
+router.post(
+  '/',
+  [
+    body('firstName').trim().isLength({ min: 2 }),
+    body('email').trim().isEmail(),
+    body('username').trim().isLength({ min: 5 }),
+    body('password').trim().isLength({ min: 6 }),
+  ],
+  userController.createUser
+);
+router.get('/:id', userController.getUser);
 
 module.exports = router;
