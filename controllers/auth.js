5b9c980180d648b94c294a43ba8e7cb57d74c2e2
Dorin <dorin.zahariazd@gmail.com>
2023-11-23 17:46:10 +0200

    Add login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -21,10 +23,47 @@ exports.signup = async (req, res, next) => {
     });
     const resultData = await user.save();
 
-    res.status(201).json({
-      message: 'User created successfully',
-      data: resultData,
+    console.log(resultData);
+
+    res.status(201).json({ id: resultData._id.toString(), email: resultData.email, username: resultData.username });
+  } catch (error) {
+    console.log(error);
+    const err = new Error('Something went wrong.');
+    err.statusCode = 500;
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    next(error);
+  }
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      const error = new Error('User not found.');
+      error.statusCode = 401;
+      next(error);
+    }
+
+    const isEqual = bcrypt.compare(req.body.password, user.password);
+
+    if (!isEqual) {
+      if (!user) {
+        const error = new Error('Wrong password.');
+        error.statusCode = 401;
+        next(error);
+      }
+    }
+    const token = jwt.sign({ email: user.email, userId: user._id.toString() }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
     });
+    res.status(200).json({ userId: user._id.toString(), token });
   } catch (error) {
     console.log(error);
     const err = new Error('Something went wrong.');
